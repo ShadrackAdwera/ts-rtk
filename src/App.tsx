4aac9d8beb2fea107200b192f06c21d944b19fe9
ShadrackAdwera <adweshshaddie@gmail.com>
2022-05-16 09:09:19 +0300

    hook up the reducer to button onclick function

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import React from 'react';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { increaseBy } from './slices/counter-slice';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const state = useAppSelector((appState) => appState.counter);
+  const dispatch = useAppDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>{`Count is ${state.value}`}</p>
+        <button onClick={() => dispatch(increaseBy(3))}>Handle Add</button>
       </header>
     </div>
   );

cb7b74a189b56b12b9770709df330e6a3350be24
ShadrackAdwera <adweshshaddie@gmail.com>
2022-05-16 07:35:27 +0300

    create counter slice

--- src/slices/counter-slice.ts
+++ src/slices/counter-slice.ts
@@ -1,1 +1,23 @@
-import {} from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface ICounterSlice {
+  value: number;
+}
+
+const initialState: ICounterSlice = { value: 0 };
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.value++;
+    },
+    increaseBy(state, action: PayloadAction<number>) {
+      state.value += action.payload;
+    },
+  },
+});
+
+export const { increaseBy, increment } = counterSlice.actions;
+export default counterSlice.reducer;
